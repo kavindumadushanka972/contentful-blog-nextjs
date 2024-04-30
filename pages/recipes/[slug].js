aa5a28e4f0762f1871719658bc18adcf1af33989
Kavindu Madushanka <kavindumadushanka972@gmail.com>
2024-04-30 17:29:30 +0530

    Feat: fallback

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,6 +1,7 @@
 import { createClient } from 'contentful';
 import Image from 'next/image';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import Skeleton from '../../components/Skeleton';
 
 const client = createClient({
   space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
@@ -18,7 +19,7 @@ export const getStaticPaths = async () => {
 
   return {
     paths,
-    fallback: false,
+    fallback: true,
   };
 };
 
@@ -35,7 +36,11 @@ export async function getStaticProps({ params }) {
 }
 
 export default function RecipeDetails({ recipe }) {
-  console.log(recipe);
+
+  if(!recipe) {
+    return <Skeleton />
+  }
+
   const { featuredImage, title, cookingTime, ingredients, method } =
     recipe.fields;
 
